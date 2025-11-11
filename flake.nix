99b5a187b28304ef2c00a9c1f0a43d2817a17910
Ladas552 <l.tokshalov@gmail.com>
2025-11-11 10:02:59 +0500

    flake: init crane incremental rebuilds, but no cargo-nextest yet

--- flake.nix
+++ flake.nix
@@ -2,56 +2,60 @@
   description = "The monolithic Norg static site generator built with Rust";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable&shallow=1";
-    flake-utils.url = "github:numtide/flake-utils?shallow=1";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = import nixpkgs {inherit system;};
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      crane,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
         toolchain = pkgs.rustPlatform;
-        cargoPackage = (pkgs.lib.importTOML "${self}/Cargo.toml").package;
-      in rec
-      {
-        # nix build
-        packages.default = toolchain.buildRustPackage {
-          pname = cargoPackage.name;
-          version = cargoPackage.version;
-          src = pkgs.lib.cleanSource "${self}";
-          cargoLock = {
-            lockFile = "${self}/Cargo.lock";
-            allowBuiltinFetchGit = true;
+        craneLib = crane.mkLib pkgs;
+        # Common arguments can be set here to avoid repeating them later
+        # Note: changes here will rebuild all dependency crates
+        commonArgs = {
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              # Default files from crane (Rust and cargo files)
+              (craneLib.fileset.commonCargoSources ./.)
+              # Also keep any files in resources
+              (pkgs.lib.fileset.maybeMissing ./src/resources)
+              (pkgs.lib.fileset.maybeMissing ./res)
+            ];
           };
-          useNextest = true;
-          dontUseCargoParallelTests = true;
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            openssl
+          ];
 
           nativeBuildInputs = with pkgs; [
             pkg-config
             perl
           ];
-          buildInputs = with pkgs; [
-            openssl
-          ];
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
-          meta = {
-            description = cargoPackage.description;
-            homepage = cargoPackage.repository;
-            license = pkgs.lib.licenses.gpl2Only;
-            maintainers = cargoPackage.authors;
-          };
-
-          # For other makeRustPlatform features see:
-          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
         };
 
+        my-crate = craneLib.buildPackage commonArgs;
+      in
+      {
+        # nix build
+        packages.default = my-crate;
+
         # nix run
-        apps.default = flake-utils.lib.mkApp {drv = packages.default;};
+        apps.default = flake-utils.lib.mkApp { drv = my-crate; };
 
         # nix develop
         devShells.default = pkgs.mkShell {
@@ -75,10 +79,6 @@
 
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         };
-      });
-
-    nixConfig = {
-      extra-substituters = ["https://ntbbloodbath.cachix.org"];
-      extra-trusted-public-keys = ["ntbbloodbath.cachix.org-1:L4DjjGwDB6O3BJ4SmtYTZbvWKLi+1v/hRlLWKOtq+f0="];
-    };
+      }
+    );
 }
